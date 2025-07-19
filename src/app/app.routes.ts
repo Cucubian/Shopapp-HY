2019fab6075a7ae41c8e4ca2a50a2ec880176143
Nguyen Duy <duy10tinhvt@gmail.com>
2025-07-20 02:01:26 +0700

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './home/home';
 import { DetailProduct } from './detail-product/detail-product';
 import { Oder } from './oder/oder';
 import { OderConfirm } from './oder-confirm/oder-confirm';
+import { Login } from './login/login';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'product-detail', component: DetailProduct},
   { path: 'oder', component: Oder},
-  { path: 'oder-confirm', component: OderConfirm}
+  { path: 'oder-confirm', component: OderConfirm},
+  { path: 'login', component: Login},
   
 ];
