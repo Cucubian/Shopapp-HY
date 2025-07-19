90ae38978581f2b86f3ab3dbad482b248f084d21
Nguyen Duy <duy10tinhvt@gmail.com>
2025-07-20 01:36:45 +0700

    oder-confim

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { DetailProduct } from './detail-product/detail-product';
 import { Oder } from './oder/oder';
+import { OderConfirm } from './oder-confirm/oder-confirm';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'product-detail', component: DetailProduct},
-  { path: 'oder', component: Oder}
+  { path: 'oder', component: Oder},
+  { path: 'oder-confirm', component: OderConfirm}
   
 ];
