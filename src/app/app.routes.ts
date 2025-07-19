581145dbcc7305c935820b8c323f05ec8d711e48
Nguyen Duy <duy10tinhvt@gmail.com>
2025-07-19 21:33:58 +0700

    tao oder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { DetailProduct } from './detail-product/detail-product';
 
 export const routes: Routes = [
-  { path: '', component: Home }
+  { path: '', component: Home },
+  { path: 'product-detail', component: DetailProduct}
+  
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
+import { DetailProduct } from './detail-product/detail-product';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, DetailProduct],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
